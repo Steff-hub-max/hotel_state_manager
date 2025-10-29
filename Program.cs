4524eaa16fc6200a6c817d42585b9df7e5fc8307
steff-hub-max <sdahlberg858@gmail.com>
2025-10-29 15:43:05 +0100

    Added a menu for user.
    so the user can see what to choose from.

--- Program.cs
+++ Program.cs
@@ -8,7 +8,38 @@ while (running)
 {
     if (active_user != null)
     {
-        Console.WriteLine($"welcome {active_user.Name}");
+        try
+        {
+            Console.Clear();
+        }
+        catch { }
+        Console.WriteLine($"--Hotel State Manager--");
+        Console.WriteLine($"welcome {active_user.Name}. Please type what you want to do.\n\n");
+        Console.WriteLine("full - list of all booked rooms");
+        Console.WriteLine("empty - list of all empty rooms");
+        Console.WriteLine("new - book a guest to a room");
+        Console.WriteLine("checkout - checkout a guest from a room");
+        Console.WriteLine("closed - mark a room as unavailable");
+        Console.WriteLine("logout - logout from this session");
+        Console.WriteLine("quit - exit the program\n\n");
+        Console.Write("Enter command: ");
+        switch (Console.ReadLine())
+        {
+            case "full":
+                break;
+            case "empty":
+                break;
+            case "new":
+                break;
+            case "checkout":
+                break;
+            case "closed":
+                break;
+            case "logout":
+                break;
+            case "quit":
+                break;
+        }
     }
     else
     {
